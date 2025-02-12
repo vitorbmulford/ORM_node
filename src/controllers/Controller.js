a978efff81e90912f5e97b1d1dacee06fceef007
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2025-02-12 19:02:16 -0300

    fazendo o crud completo com o models pessoa

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,7 +1,9 @@
+
 class Controller {
   constructor(entidadeService) {
     this.entidadeService = entidadeService;
   }
+
   async pegaTodos(req, res) {
     try {
       const listaDeRegistros = await this.entidadeService.getAllRegister();
@@ -10,5 +12,70 @@ class Controller {
       res.status(500).json({ erro: "Erro interno do servidor" });
     }
   }
+  async pegaUmPorId(req, res) {
+    const { id } = req.params;
+    try {
+      const umRegistro = await this.entidadeService.pegaUmRegistroPorId(
+        Number(id)
+      );
+
+      if (!umRegistro) {
+        return res.status(400).json({ mensagem: "Id não encontrado" });
+      }
+      return res.status(200).json(umRegistro);
+    } catch {
+      res.status(500).json({ erro: "Erro interno do servidor" });
+    }
+  }
+  async criaNovo(req, res) {
+    try {
+      const dadosParaCriacao = req.body;
+      const novoRegistroCriado = await this.entidadeService.criarRegistro(
+        dadosParaCriacao
+      );
+
+      if (!novoRegistroCriado) {
+        return res.status(400).json({ mensagem: "Erro ao criar" });
+      }
+
+      return res.status(201).json(novoRegistroCriado); 
+      
+    } catch (error) {
+      console.error("Erro ao criar registro:", error);
+      res.status(500).json({ erro: "Erro interno do servidor" });
+    }
+  }
+
+  async atualiza(req, res) {
+    const { id } = req.params;
+    const dadosAtualizados = req.body;
+    try {
+      const foiAtualizado = await this.entidadeService.atualizaRegistro(
+        dadosAtualizados,
+        Number(id)
+      );
+      if (!foiAtualizado) {
+        return res
+          .status(400)
+          .json({ mensagem: "Registro não foi atualizado" });
+      }
+      return res.status(200).json({ mensagem: "Atualizado com sucesso" });
+    } catch (erro) {
+      console.error("Erro ao atualizar registro:", erro);
+      res.status(500).json({ erro: "Erro interno do servidor" });
+    }
+  }
+  async exclui(req, res) {
+    const { id } = req.params;
+    try {
+      await this.entidadeService.excluiRegistro(Number(id));
+      return res
+        .status(200)
+        .json({ mensagem: `Id ${id} deletado com sucesso` });
+    } catch {
+      res.status(500).json({ erro: "Erro interno do servidor" });
+    }
+  }
 }
+
 module.exports = Controller;
--- src/services/Services.js
+++ src/services/Services.js
@@ -7,5 +7,28 @@ class Services {
   async getAllRegister() {
     return dataSource[this.model].findAll();
   }
-} 
+
+  async pegaUmRegistroPorId(id) {
+    return dataSource[this.model].findByPk(id);
+  }
+
+  async criarRegistro(dadosDoRegistro) {
+    return dataSource[this.model].create(dadosDoRegistro);
+  }
+
+  async atualizaRegistro(dadosAtualizados, id) {
+    const [registrosAtualizados] = await dataSource[this.model].update(
+      dadosAtualizados,
+      { where: { id } }
+    );
+
+    return registrosAtualizados > 0;
+  }
+  async excluiRegistro(id) {
+    const deletado = await dataSource[this.model].destroy({
+      where: { id },
+    });
+    return deletado > 0;
+  }
+}
 module.exports = Services;
