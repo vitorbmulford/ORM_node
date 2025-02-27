4c601d627eae743a9fe68b7f927939f41603d39e
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2025-02-27 18:59:27 -0300

    adicionando paranoid

--- src/database/models/matriculas.js
+++ src/database/models/matriculas.js
@@ -19,6 +19,7 @@ module.exports = (sequelize, DataTypes) => {
       sequelize,
       modelName: "Matriculas",
       tableName: "matriculas",
+      paranoid: true,
     }
   );
   return Matriculas;
--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -25,6 +25,7 @@ module.exports = (sequelize, DataTypes) => {
       sequelize,
       modelName: "Pessoa",
       tableName: "pessoas",
+      paranoid: true,
     }
   );
   return Pessoa;
